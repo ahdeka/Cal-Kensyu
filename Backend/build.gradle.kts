cc420e81db42f69d98889bd7de7a59632a850cec
ahdeka <ahdeka01@gmail.com>
2025-12-26 16:52:00 +0900

    [Backend] Docker Setting Complete

--- Backend/build.gradle.kts
+++ Backend/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.12.3")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
+    runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.security:spring-security-test")
     implementation("com.opencsv:opencsv:5.9")
 }
